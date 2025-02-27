db198e358374eaf728f455b2686fc73cfbab598c
hardik singh <realhardik18@gmail.com>
2025-02-27 16:20:43 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,12 +10,6 @@ export default function Navbar() {
     >
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <div className="text-white text-2xl font-bold">Kozu</div>
-        <div className="flex items-center gap-6">
-          <a href="#" className="text-white hover:text-blue-400 transition">Home</a>
-          <a href="#" className="text-white hover:text-blue-400 transition">Features</a>
-          <a href="#" className="text-white hover:text-blue-400 transition">Platforms</a>
-          <a href="#" className="text-white hover:text-blue-400 transition">Contact</a>
-        </div>
       </div>
     </motion.nav>
   )
