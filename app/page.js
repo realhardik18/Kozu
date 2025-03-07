2289f9ccaf71a5651372fd6c44a4b997b0b69ad7
hardik singh <realhardik18@gmail.com>
2025-03-07 17:01:24 +0530

    init nextjs

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='flex-row items-centerm text-center pt-12'>
+        <div className='text-white text-3xl'>Welcome to kozu</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
